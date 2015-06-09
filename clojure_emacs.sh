1cb91e58b6ec35c929a1165d0139ead8136751cc
ofiesh <nofiesh@build.com>
2015-06-08 17:08:53 -0700

    initial commit

---
+++ clojure_emacs.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install openjdk-7-jre openjdk-7-jdk emacs24 git -y
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+sudo cp lein /usr/local/bin/lein-install
+sudo chmod a+x /usr/local/bin/lein
+set LEIN_ROOT
+lein
+git clone https://github.com/flyingmachine/emacs-for-clojure.git ~/.emacs.d
+rm -fr ~/.emacs.d/.git
+echo '{:user {:plugins [[cider/cider-nrepl "0.8.1"]]}}' >> ~/.lein/profiles.clj
